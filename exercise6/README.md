222cbcf0b36da014d7ffb9c3f523b48dfbd55e8a
JRonkin <41006960+JRonkin@users.noreply.github.com>
2024-06-08 12:02:31 -0400

    add exercise 6 part 2

--- exercise6/README.md
+++ exercise6/README.md
@@ -2,4 +2,32 @@
 
 This is going to be a multi-part exercise to teach about classes. We're going to make a Tic Tac Toe game.
 
+## Part 1
+
 For the first part, we'll make the basic class and add the ability to set squares on the board to either X or O, as well as reset the board back to empty. The tests currently in index.spec.js are only for this part, so make sure they all pass.
+
+## Part 2
+
+For the second part, we're going to add the game logic: keeping track of turns, checking whether a move is valid, and detecting a win or tie. Our Tic Tac Toe game will have the standard rules, which are:
+- X always goes first.
+- Players alternate turns, so after X selects a square, it becomes O's turn to select a square, and then back to X.
+- If one player gets 3 squares in a row, column, or diagonal, the game ends and that player wins.
+- If all squares are filled and no player has 3-in-a-row, the game ends in a tie.
+
+Add and implement the following methods on the TicTacToe class:
+
+### TicTacToe.playerMove(row, col)
+
+This method should set the mark of the player whose turn it is at the given square. For example, calling `ticTacToe.playerMove(1, 1)` as the first move would set an "X" in the center, then calling `ticTacToe.playerMove(0, 2)` would set an "O" in the top right corner. It should also check to make sure the move is valid, and if not, it should do nothing. For example, `ticTacToe.playerMove(0, 4)` would be invalid because it's outside the board. Trying to move in a square that already has "X" or "O" would also be invalid.
+
+### TicTacToe.getCurrentPlayer()
+
+This method takes no arguments and returns the mark of the player whose turn it is, either "X" or "O".
+
+### TicTacToe.isGameOver()
+
+This method takes no arguments and returns true if the game is over, either due to one player winning or a tie. Otherwise, it returns false.
+
+### TicTacToe.getWinner()
+
+This method takes no arguments and returns the mark of the player who won the game, "X" or "O". If no player has won, it returns an empty string "".
--- exercise6/index.spec.js
+++ exercise6/index.spec.js
@@ -55,3 +55,137 @@ describe('TicTacToe.reset', function () {
     expect(getBoard(ticTacToe)).to.deep.equal([["", "", ""], ["", "", ""], ["", "", ""]]);
   });
 });
+
+describe('TicTacToe.playerMove', function () {
+  const ticTacToe = new TicTacToe();
+
+  it('should set the square for the current player, starting with X', function() {
+    ticTacToe.playerMove(0, 0);
+    expect(getBoard(ticTacToe)).to.deep.equal([["X", "", ""], ["", "", ""], ["", "", ""]]);
+  });
+
+  it('should set the square for the next player, after the first move', function() {
+    ticTacToe.playerMove(1, 1);
+    expect(getBoard(ticTacToe)).to.deep.equal([["X", "", ""], ["", "O", ""], ["", "", ""]]);
+  });
+
+  it('should set the square for the next player, after the second move', function() {
+    ticTacToe.playerMove(0, 1);
+    expect(getBoard(ticTacToe)).to.deep.equal([["X", "X", ""], ["", "O", ""], ["", "", ""]]);
+  });
+
+  it('should not set an invalid square', function() {
+    ticTacToe.playerMove(0, 4);
+    ticTacToe.playerMove(-1, 1);
+    ticTacToe.playerMove(5, 0);
+    expect(getBoard(ticTacToe)).to.deep.equal([["X", "X", ""], ["", "O", ""], ["", "", ""]]);
+  });
+
+  it('should not set a square that is already taken', function() {
+    ticTacToe.playerMove(0, 0);
+    ticTacToe.playerMove(1, 1);
+    expect(getBoard(ticTacToe)).to.deep.equal([["X", "X", ""], ["", "O", ""], ["", "", ""]]);
+  });
+});
+
+describe('TicTacToe.getCurrentPlayer', function () {
+  const ticTacToe = new TicTacToe();
+
+  it('should get X for the first turn', function() {
+    expect(ticTacToe.getCurrentPlayer()).to.equal('X');
+  });
+
+  it('should get O for the second turn', function() {
+    ticTacToe.playerMove(0, 0);
+    expect(ticTacToe.getCurrentPlayer()).to.equal('O');
+  });
+
+  it('should get X for the third turn', function() {
+    ticTacToe.playerMove(1, 1);
+    expect(ticTacToe.getCurrentPlayer()).to.equal('X');
+  });
+
+  it('should not change players after an invalid move', function() {
+    ticTacToe.playerMove(0, 0);
+    expect(ticTacToe.getCurrentPlayer()).to.equal('X');
+  });
+});
+
+describe('TicTacToe.isGameOver', function () {
+  const ticTacToe = new TicTacToe();
+
+  it('should not be over at the start', function() {
+    expect(ticTacToe.isGameOver()).to.be.false;
+  });
+
+  it('should be over when the board is full', function() {
+    ticTacToe.playerMove(0, 0);
+    ticTacToe.playerMove(0, 1);
+    ticTacToe.playerMove(0, 2);
+    ticTacToe.playerMove(1, 0);
+    ticTacToe.playerMove(1, 1);
+    ticTacToe.playerMove(1, 2);
+    ticTacToe.playerMove(2, 0);
+    ticTacToe.playerMove(2, 1);
+    ticTacToe.playerMove(2, 2);
+    expect(ticTacToe.isGameOver()).to.be.true;
+  });
+
+  it('should be over when there is a winner', function() {
+    ticTacToe.reset();
+    ticTacToe.playerMove(0, 0);
+    ticTacToe.playerMove(1, 1);
+    ticTacToe.playerMove(0, 1);
+    ticTacToe.playerMove(1, 2);
+    ticTacToe.playerMove(0, 2);
+    expect(ticTacToe.isGameOver()).to.be.true;
+  });
+});
+
+describe('TicTacToe.getWinner', function () {
+  const ticTacToe = new TicTacToe();
+
+  it('should not have a winner at the start', function() {
+    expect(ticTacToe.getWinner()).to.equal('');
+  });
+
+  it('should have a winner when there is a horizontal row', function() {
+    ticTacToe.reset();
+    ticTacToe.playerMove(0, 0);
+    ticTacToe.playerMove(1, 1);
+    ticTacToe.playerMove(0, 1);
+    ticTacToe.playerMove(1, 2);
+    ticTacToe.playerMove(0, 2);
+    expect(ticTacToe.getWinner()).to.equal('X');
+  });
+
+  it('should have a winner when there is a vertical column', function() {
+    ticTacToe.reset();
+    ticTacToe.playerMove(2, 2);
+    ticTacToe.playerMove(0, 0);
+    ticTacToe.playerMove(1, 1);
+    ticTacToe.playerMove(1, 0);
+    ticTacToe.playerMove(1, 2);
+    ticTacToe.playerMove(2, 0);
+    expect(ticTacToe.getWinner()).to.equal('O');
+  });
+
+  it('should have a winner when there is a diagonal', function() {
+    ticTacToe.reset();
+    ticTacToe.playerMove(0, 0);
+    ticTacToe.playerMove(1, 0);
+    ticTacToe.playerMove(1, 1);
+    ticTacToe.playerMove(1, 2);
+    ticTacToe.playerMove(2, 2);
+    expect(ticTacToe.getWinner()).to.equal('X');
+  });
+
+  it('should not have a winner when there is no winner', function() {
+    ticTacToe.reset();
+    ticTacToe.playerMove(0, 0);
+    ticTacToe.playerMove(1, 1);
+    ticTacToe.playerMove(0, 1);
+    ticTacToe.playerMove(1, 2);
+    expect(ticTacToe.getWinner()).to.equal('');
+  });
+});
